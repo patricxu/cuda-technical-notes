effb9d0e1cfbcfd5a16f38645ea6af1c471e0584
patricxu <qxu000@163.com>
2024-05-14 21:32:40 +0800

    bank conflict

--- cudabasics/bankconflict/bankconflict.cu
+++ cudabasics/bankconflict/bankconflict.cu
@@ -16,71 +16,46 @@
     } while(0)
 
 
-__global__ void kernel(int stride, int* out) {
-    volatile int sharedArray[SHM_NUM];
+__global__ void kernel(int stride, unsigned long long *d_out) {
+    __shared__ int sharedArray[SHM_NUM];
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    // Each thread loads its corresponding element into shared memory
-    for (int i = 0; i < SHM_NUM/N; i++) {
-        sharedArray[tid * SHM_NUM/N + i] = 1;
-    }
-    __syncthreads();
+    unsigned long long startClock = clock64();
+    sharedArray[tid * stride]++;
+    unsigned long long endClock = clock64();
 
-    // Simulate a read operation from shared memory
-    for (int i = 0; i < 10000; ++i) {
-        out[tid] = sharedArray[tid] + sharedArray[(tid + stride) & (SHM_NUM-1)];
-    }
+    d_out[tid] = endClock - startClock;
 }
 
 int main(int argc, char** argv) {
-    int h_out[N];
-    auto [stride, iteration] = parseCommandLineArguments(argc, argv);
+    unsigned long long h_out[N];
+    auto [stride, _] = parseCommandLineArguments(argc, argv);
 
-    printf("begin with stride=%d, iteration=%d\n", stride, iteration);
+    printf("begin with stride=%d\n", stride);
 
-    // query shared memory bank size
-    cudaSharedMemConfig sharedMemConfig;
-
-    // set it to four, just in case
-    CHECK_CUDA_ERROR(cudaDeviceGetSharedMemConfig(&sharedMemConfig));
-    printf("sharedMemConfig = %d\n", sharedMemConfig);
-
-    int *d_out;
-    CHECK_CUDA_ERROR(cudaMalloc((void**)&d_out, N * sizeof(int)));
+    unsigned long long *d_out;
+    CHECK_CUDA_ERROR(cudaMalloc((void**)&d_out, N * sizeof(unsigned long long)));
 
     dim3 threadsPerBlock(N);
     dim3 numBlocks(1, 1);
 
-    cudaEvent_t start, end;
-    CHECK_CUDA_ERROR(cudaEventCreate(&start));
-    CHECK_CUDA_ERROR(cudaEventCreate(&end));
+    kernel<<<numBlocks, threadsPerBlock>>>(stride, d_out);
+    cudaError_t err = cudaGetLastError();
 
-    CHECK_CUDA_ERROR(cudaEventRecord(start));
-    for (int i = 0; i < iteration; i++){
-        kernel<<<numBlocks, threadsPerBlock>>>(stride, d_out);
-        cudaError_t err = cudaGetLastError();
-
-        if (err != cudaSuccess){
-            fprintf(stderr, "Failed to launch convert kernel (error code %s)!\n", cudaGetErrorString(err));
-            exit(EXIT_FAILURE);
-        }
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to launch convert kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
     }
-    CHECK_CUDA_ERROR(cudaMemcpy((void*)h_out, (void*)d_out, N * sizeof(int), cudaMemcpyDeviceToHost));
 
-    CHECK_CUDA_ERROR(cudaEventRecord(end));
-    CHECK_CUDA_ERROR(cudaEventSynchronize(end));
+    cudaDeviceSynchronize();
+    CHECK_CUDA_ERROR(cudaMemcpy((void*)h_out, (void*)d_out, N * sizeof(unsigned long long), cudaMemcpyDeviceToHost));
 
-    // for(int i=0; i<N; i++)
-    // {
-    //     printf("h_out[%d] = %d\n", i, h_out[i]);
-    // }
-    float milliseconds = 0;
-    CHECK_CUDA_ERROR(cudaEventElapsedTime(&milliseconds, start, end));
-    printf("kernel excution time total=%f, avg=%f ms\n", milliseconds, milliseconds/iteration);
 
-    CHECK_CUDA_ERROR(cudaEventDestroy(start));
-    CHECK_CUDA_ERROR(cudaEventDestroy(end));
+    for(int i=0; i<N; i++)
+    {
+        printf("h_out[%d] = %llu\n", i, h_out[i]);
+    }
+    
 
     return 0;
 }
